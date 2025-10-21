213f8b404854a855420c4c256450c4927e74752f
dwi cahya <dwiramdahan12@gmail.com>
2025-10-21 09:23:39 +0100

    Add files via upload

---
+++ latihan java s/latihan.html/script.js
@@ -0,0 +1,7 @@
+function kirim(){
+    let namadepan = document.getElementById("namadepan").value
+    let namabelakang = document.getElementById("namabelakang").value
+    console.log("nama lengkap: "+ namadepan +" " + namabelakang)
+}
+
+
